6d19b07807d1388b74e9bdebfa422ee49e8c6d6e
balint21100 <ivanics.balint21@gmail.com>
2024-12-07 21:10:50 +0100

    test

--- L54E2T_HSZF_2024251.Test/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace L54E2T_HSZF_2024251.Test
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ L54E2T_HSZF_2024251.Test/TestData.cs
@@ -0,0 +1,27 @@
+﻿using L54E2T_HSZF_2024251.Application;
+using L54E2T_HSZF_2024251.Model;
+
+namespace L54E2T_HSZF_2024251.Test
+{
+    public class TestData
+    {
+        public static Pharaohs p = new Pharaohs
+        {
+            Id = 1,
+            Name = "Ramses",
+            Reign_Start = Convert.ToDateTime("2002-02-12"),
+            Reign_End = Convert.ToDateTime("2002-08-12"),
+            
+            
+        };
+        public static Pharaohs wrongp = new Pharaohs
+        {
+            Id = 1,
+            Name = "Ramses23",
+            Reign_Start = Convert.ToDateTime("2002-08-12"),
+            Reign_End = Convert.ToDateTime("2002-02-12"),
+
+
+        };
+    }
+}
